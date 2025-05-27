f7c28daaf71aaa4decefb3b85d3e6b867e04964d
Gabriel Alderige <galbyoficial@gmail.com>
2025-05-27 14:54:11 -0300

    final

--- script.js
+++ script.js
@@ -1,9 +1,13 @@
 let imagem = document.getElementById("troca-imagem")
 
 function trocaImagem(){
-    imagem.style.opacity = 0
+    if(imagem.style.opacity == 0) {
+        imagem.style.opacity = 1
+    } else{
+        imagem.style.opacity=0
+    }
 }
 
 setInterval(() => {
     trocaImagem()
-}, 3000)
+}, 4000)
